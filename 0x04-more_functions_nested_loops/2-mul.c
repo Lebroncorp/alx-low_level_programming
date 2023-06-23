beb12fc33a3540509a7338cb352f28b056445752
lebron <lebroncorp@yahoo.com>
2023-06-23 13:17:26 -0700

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * mul - multiply
+ *
+ * Description: multiplies
+ * two numbers.
+ * @a: int type
+ * @b: int type
+ *
+ * Return: int
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return(mul);
+}
