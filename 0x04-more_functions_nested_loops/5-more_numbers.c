188b1f5172ddc793d38eee023ed21344dc7b9705
lebron <lebroncorp@yahoo.com>
2023-06-24 19:13:29 -0700

    print numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,23 +10,19 @@ void more_numbers(void)
 {
 	int line;
 	int num;
-	int first_num;
-	int second_num;
 
 	for (line = 0; line < 10; line++)
 	{
 		for (num = 0; num < 15; num++)
 		{
-			first_num = num / 10;
-			second_num = num % 10;
 			if (num > 9)
 			{
-				_putchar(first_num + '0');
-				_putchar(second_num + '0');
+				_putchar(1);
+				_putchar(num % 10 + '0');
 			}
 			else
 			{
-				_putchar(second_num + '0');
+				_putchar(num + '0');
 			}
 		}
 		_putchar('\n');
