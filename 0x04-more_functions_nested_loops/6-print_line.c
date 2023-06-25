fb5da83beaeacd0ff103e51cbd2f742b41f36735
lebron <lebroncorp@yahoo.com>
2023-06-24 18:13:18 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
 /**
+ * print_line - prints line
  *
+ * Description: prints line the number of times asked
+ * @n: int type
  */
 
 void print_line(int n)
 {
 	int times;
-	
+
 	times = n + 1;
 	for (n = 0; n < times; n++)
 	{
