f0ab7ceeae2f24dffb199b06f839231abd6e5c59
lebron <lebroncorp@yahoo.com>
2023-06-27 14:39:05 -0700

    string copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strcpy - copies string
+ *
+ * @dest: char type
+ * @src: char type
+ *
+ * Return: char 
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
