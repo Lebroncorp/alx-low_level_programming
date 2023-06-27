0bb9aa0d7433fda228e97071080b4884e571e1a3
lebron <lebroncorp@yahoo.com>
2023-06-27 15:46:22 -0700

    main tasks

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,27 @@
-#include "main.h"
+#include "main.h
 
-/**
+"/**
+ * rev_string - reverses a string
  *
+ * Description: reverses a string. does not
+ * print to stdout and returns nothing
+ * @s: string input
+ *
+ * Return: nothing
  */
 
 void rev_string(char *s)
 {
+	int i, j, count = 0;
+	char temp;
 
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+
+	for (i = 0, j = count - 1; i < j; i++, j--)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
+}
