6aa68b3b08ba39c8c8e87f7f10c6d9728b94d30c
lebron <lebroncorp@yahoo.com>
2023-06-27 08:45:18 -0700

    print string backwards

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - prints string backwards
+ *
+ * @s: type character
+ */
+
+void print_rev(char *s)
+{
+	int a;
+	int length;
+
+	length = strlen(s);
+
+	for (a = length; a >= 0; a--)
+		_putchar(s[a]);
+}
