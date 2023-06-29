52ff017d98e12a1caadf8956961cdb9b807c3d9a
lebron <lebroncorp@yahoo.com>
2023-06-29 14:36:11 -0700

    string cat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,10 +2,11 @@
 #include <string.h>
 
 /**
- * *_strcat - cocatenates two strings
+ * _strcat - cocatenates two strings
  *
- * @*dest: char type
- * @*src: char type
+ * @dest: char type
+ * @src: char type
+ * Return: dest char type
  */
 
 char *_strcat(char *dest, char *src)
