cd2eac72722083f7b007e4afd5b594971cd21d6d
lebron <lebroncorp@yahoo.com>
2023-06-29 19:47:04 -0700

    question 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,24 @@
 #include "main.h"
 
 /**
+ * string_toupper - changes lower
+ *  case letteres to upper case
  *
+ *  @str: char type
+ *  Return: str char type
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] -= 32;
+
+		index++;
+	}
+
+	return (str);
+}
