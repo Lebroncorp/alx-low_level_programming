49a30975a6c641bde91abc77cdf926ec16d98550
lebron <lebroncorp@yahoo.com>
2023-06-29 19:29:19 -0700

    quest 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,25 @@
 #include "main.h"
 
 /**
+ * _strcmp - compares string
  *
+ * @s1: char type
+ * @s2: char type
  */
 
 int _strcmp(char *s1, char *s2)
+{
+	int i, j;
+
+	j = 0;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+	{
+		j = s1[i] - s2[i];
+		break;
+	}
+	}
+		return (j);
+}
