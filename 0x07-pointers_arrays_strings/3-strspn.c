4cdf5494837fb1d0c0fd17d6fb4787671047c606
lebron <lebroncorp@yahoo.com>
2023-07-04 12:14:06 -0700

    3-string substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,5 +11,20 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
+	int a;
+	int b;
+	unsigned int count = 1;
 
+	for(a = 0; accept[a] != '\0'; a++)
+	{
+		for(b = 0; s[b] != '\0'; b++)
+		{
+			if(accept[a] == s[b])
+			{
+				count++;
+				break;
+			}
+		}
+	}
+	return (count);
 }
