a7fbadf4a1787d5f0e8d919e014a04239a963b77
lebron <lebroncorp@yahoo.com>
2023-07-04 15:40:39 -0700

    pointer to pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - pointer that points to a pointer
+ *
+ * @s: char type pointer to pointer
+ * @to: char type pointer
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
