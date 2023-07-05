056be5b263b347974dc468ccc7fbfafc15a1f559
lebron <lebroncorp@yahoo.com>
2023-07-04 18:50:20 -0700

    j

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 #include <stdio.h>
 
 /**
@@ -11,5 +12,23 @@
 
 void print_diagsums(int *a, int size)
 {
+	int i, j;
+	int sum1 = 0;
+	int sum2 = 0;
 
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+			{
+				sum1 += *(a + i * size + j);
+			}
+			if (i == size - 1 - j) {
+				sum2 += *(a + i * size + j);
+			}
+		}
+	}
+	printf("Sum of the main diagonal: %d\n", sum1);
+	printf("Sum of the secondary diagonal: %d\n", sum2);
 }
