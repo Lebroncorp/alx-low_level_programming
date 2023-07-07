f0ee44b04885c3e3fe149902e004faf539e22c5b
Lebroncorp <lebroncorp@yahoo.com>
2023-07-07 13:44:48 -0700

    library

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,10 +1,16 @@
 #include "main.h"
 
+/**
+ * _puts - prints strings
+ *
+ * @s: char type pointer or string
+ */
+
 void _puts(char *s)
 {
 	int n;
 
-	for (n = 0; s[n] != '\0'; n++)
+	for (n = 0; s[n] < '\0'; n++)
 	{
 		_putchar(s[n]);
 	}
