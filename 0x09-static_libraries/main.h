49c0c4596890dd3a94147dcb457b546f74142fc9
Lebroncorp <lebroncorp@yahoo.com>
2023-07-07 15:00:32 -0700

    l

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ *
+ * Author: K.L.U
+ *
+ * Description: Header file containing the prototypes of all functions
+ */
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -18,3 +29,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+#endif
