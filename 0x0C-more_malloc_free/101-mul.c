af67f4505ccdbf9a2fdf3c53fa63de385b6e7ed4
Lebroncorp <lebroncorp@yahoo.com>
2023-07-13 16:32:29 -0700

    jk

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - multiplies two positive numbers
+ *
+ * @argc: argv count
+ * @argv: argument variable
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
