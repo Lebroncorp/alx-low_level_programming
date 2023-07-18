861c6a8369b8dc9b6632d293382e64cebc729aad
Lebroncorp <lebroncorp@yahoo.com>
2023-07-17 21:01:04 -0700

    k

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - function that frees dogs
+ * @d: dog of type dog_t
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name != NULL)
+	{
+		free(d->name);
+	}
+	if (d->owner != NULL)
+	{
+		free(d->owner);
+	}
+	free(d);
+}
