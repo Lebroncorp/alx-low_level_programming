5ecfac350e983ca7b3254f99fb6a09bd201f7487
Lebroncorp <lebroncorp@yahoo.com>
2023-07-20 20:03:46 -0700

    h

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - prints numbers
+ *
+ * @separator: char * constant
+ * @n: unsigned int constant
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list arg_list;
+	unsigned int i;
+	unsigned int x;
+
+	va_start(arg_list, n);
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(arg_list, signed int);
+		printf("%d", x);
+
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	putchar('\n');
+	va_end(arg_list);
+}
