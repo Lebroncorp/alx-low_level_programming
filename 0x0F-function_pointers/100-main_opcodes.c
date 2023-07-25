d2faf6106ed96eb99e3ba429c7701fef11b3ecce
Lebroncorp <lebroncorp@yahoo.com>
2023-07-24 18:30:10 -0700

    g

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,47 +1,43 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * print_opcodes - prints opcodes
+ * main - Prints the opcodes of itself.
  *
- * @opcde: int argument
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * Return: nothing
- */
-
-void print_opcodes(int opcde)
-{
-	int n;
-	unsigned char *ptr;
-
-	ptr = (unsigned char *)print_opcodes;
-	for (n = 0; n < opcde; n++)
-		printf("%02x ", ptr[n]);
-	printf("\n");
-}
-
-
-/**
- * main - entry point
- *
- * @argc: argument count
- * @argv: array of arguments
- *
- * Return: int
+ * Return: Always 0.
  */
 
 int main(int argc, char *argv[])
 {
+	int bytes;
+	int index;
+	int (*address)(int, char **) = main;
+	unsigned char opcode;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	if (atoi(argv[1]) < 0)
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	print_opcodes(atoi(argv[1]));
+	for (index = 0; index < bytes; index++)
+	{
+		opcode = *(unsigned char *)address;
+		printf("%.2x", opcode);
+		if (index == bytes - 1)
+			continue;
+		printf(" ");
+		address++;
+	}
+	printf("\n");
+
 	return (0);
 }
