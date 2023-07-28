fb8f3c539c71f996d46350117606bcf165684b08
Lebroncorp <lebroncorp@yahoo.com>
2023-07-27 19:52:17 -0700

    tr

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,6 +2,14 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * print_list - prints all the elements of a list_t list
+ *
+ * @h: the linked list list_t
+ *
+ * Return: The number of nodes in h
+ */
+
 size_t print_list(const list_t *h)
 {
 	int count = 0;
@@ -15,7 +23,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		count ++;
+		count++;
 	}
 	return (count);
 
