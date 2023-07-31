cc0cb0cbc48c31d3c040c447d5c5ea3d828efd67
Lebroncorp <lebroncorp@yahoo.com>
2023-07-31 15:07:49 -0700

    f

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,17 +7,29 @@
  */
 
 void free_listint2(listint_t **head)
-{
+/**{
 	listint_t *temp;
 
-	if (*head == NULL)
-		return;
 	while (*head != NULL)
 	{
 		temp = *head;
 		*head = (*head)->next;
 		free(temp);
+	}	
+}
+*/
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
 	}
+
 	head = NULL;
-	
 }
