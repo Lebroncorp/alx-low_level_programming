937b205a3902ce10caadec03722d06b347a465c8
Lebroncorp <lebroncorp@yahoo.com>
2023-07-31 17:14:06 -0700

    d

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,6 +13,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int n = 0;
 
+	if (head == NULL)
+		return (NULL);
+
 	while (head != NULL)
 	{
 		head = head->next;
