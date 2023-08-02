700c43903a306d0799f4c6906958229ee3dd63c0
Lebroncorp <lebroncorp@yahoo.com>
2023-08-01 17:02:11 -0700

    s

--- 0x13-more_singly_linked_lists/revision.c
+++ 0x13-more_singly_linked_lists/revision.c
@@ -16,12 +16,21 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	temp = *head;
-	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		if (idx == 0)
+		{
+			*head = new_node;
+			new_node->next = NULL;
+			return (new_node);
+		}
+		else
+		return (NULL);
+	}
 	if (idx == 0)
 	{
-		new_node->next = temp;
-		temp = new_node;
+		new_node->next = *head;
+		*head = new_node;
 		return (new_node);
 	}
 	for ( ; *head != NULL; *head = (*head)->next)
@@ -34,6 +43,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		idx++;
 	}
+	temp = *head;
 	if (temp->next == NULL)
 	{
 		new_node = temp->next;
