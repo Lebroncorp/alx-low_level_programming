fbdd4c1721b50f1d22c248248b7bffde23be0126
Lebroncorp <lebroncorp@yahoo.com>
2023-08-01 21:39:27 -0700

    a

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * beforeMain - function that prints before main
+ *
+ * DescriptionApply the constructor attribute
+ * to myStartupFun() so that it is executed before main()
+ */
+
+void beforeMain(void) __attribute__ ((constructor));
+
+void beforeMain(void)
+{
+	printf
+		("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
