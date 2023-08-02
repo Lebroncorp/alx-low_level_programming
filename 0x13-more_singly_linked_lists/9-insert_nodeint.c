8e35f957c0b5a2c27dcbe8e455e2d918855382c3
Lebroncorp <lebroncorp@yahoo.com>
2023-08-01 21:29:25 -0700

    a

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,19 +11,21 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node;
-	listint_t *temp;
+	listint_t *temp = *head;
+	listint_t *prev;
 	unsigned int node = 0;
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
+	prev = *head;
+	new_node->next = NULL;
 	if (*head == NULL)
 	{
 		if (idx == 0)
 		{
 			*head = new_node;
-			new_node->next = NULL;
 			return (new_node);
 		}
 		else
@@ -31,25 +33,24 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	if (idx == 0)
 	{
-		new_node->next = *head;
-		*head = new_node;
+		new_node->next = temp;
+		temp = new_node;
 		return (new_node);
 	}
-	for ( ; *head != NULL; *head = (*head)->next)
+	for ( ; temp->next != NULL; temp = temp->next)
 	{
 		if (node < idx)
 		{
-			new_node->next = (*head)->next;
-			(*head)->next = new_node;
+			new_node->next = temp->next;
+			temp->next = new_node;
 			return (new_node);
 		}
 		node++;
 	}
-	temp = *head;
+
 	if (temp->next == NULL)
 	{
 		new_node = temp->next;
-		new_node->next = NULL;
 	}
 	return (new_node);
 }
