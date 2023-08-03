3ea355ef479824aebd318e9954a990760696b114
Lebroncorp <lebroncorp@yahoo.com>
2023-08-03 16:35:56 -0700

    ready

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ *
+ * @n: the decimal number to be converted to binary
+ */
+
+void print_binary(unsigned long int n)
+{
+}
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * flip_bits - returns the number of bits you
+ * would need to flip to get from one number to another.
+ *
+ * @n: the number to be flipped to bits.
+ * @m: the number to be flipped too.
+ *
+ * Return: unsigned int of bits you would
+ * need to flip to get from one number to another.
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+}
