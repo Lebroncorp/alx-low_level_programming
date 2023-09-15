3d49fde2169ef43dfec539047585c7c94f2fb132
Lebroncorp <lebroncorp@yahoo.com>
2023-09-14 19:15:15 -0700

    free

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,5 +1,22 @@
 #include "lists.h"
 
+/**
+ * get_dnodeint_at_index -gets the node at given index
+ * @head: head pointer to list
+ * @index: index of node to be retrieved
+ *
+ * Return: retrieved node or NULL if it doesn't exist
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	size_t i = 0;
+
+	while (head != NULL)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+		i++;
+	}
+	return (NULL);
 }
--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,5 +1,19 @@
 #include "lists.h"
 
+/**
+ * sum_dlistint - sums all the data in given list
+ * @head: head pointer of list
+ *
+ * Return: sum of data in list
+ */
 int sum_dlistint(dlistint_t *head)
 {
+	int result = 0;
+
+	while (head != NULL)
+	{
+		result += head->n;
+		head = head->next;
+	}
+	return (result);
 }
